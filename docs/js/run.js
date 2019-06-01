8a9bbd958964f7ca14dde0d9e9408c54e4fff4c8
Alexandr Cheremisin <an-cheremisin@yandex.ru>
2019-06-02 02:32:23 +0300

    Доработал модальное окно формы

--- docs/js/run.js
+++ docs/js/run.js
@@ -5,9 +5,10 @@ window.onload = function () {
   const overlay = document.querySelector('.wrapper');
   const menuList = document.querySelector('.menu__list');
   const teamsList = document.querySelector('.teams__list');
-  const menuClose = menuList.querySelectorAll('.menu__desc-close')
+  const menuClose = menuList.querySelectorAll('.menu__desc-close');
   const ESC_KEYCODE = 27;
   const ENTER_KEYCODE = 13;
+
 
   // Открыть/закрыть главное меню
   function openMenu() {
@@ -63,22 +64,129 @@ window.onload = function () {
     }
   };
   menuList.addEventListener('click', openMenuItem);
-  teamsList.addEventListener('click', openTeamsInfo);
-  
   for (let i = 0; i < menuClose.length; i++) {
     menuClose[i].addEventListener('click', closeMenuItem)
   }
-};
 
-////////////////////////////////////////////////////////////////////////
-function openTeamsInfo(e) {
-  e.target.parentElement.classList.toggle('open');
-  const teamId = localStorage.getItem('teamId');
-  localStorage.setItem('teamId', e.target.parentElement.id);
-
-  if (teamId != e.target.parentElement.id) {
-    document.getElementById(teamId).classList.remove('open');
-    localStorage.removeItem('teamId');
+  function openTeamsInfo(e) {
+    e.target.parentElement.classList.toggle('open');
+    const teamId = localStorage.getItem('teamId');
     localStorage.setItem('teamId', e.target.parentElement.id);
+
+    if (teamId != e.target.parentElement.id) {
+      document.getElementById(teamId).classList.remove('open');
+      localStorage.removeItem('teamId');
+      localStorage.setItem('teamId', e.target.parentElement.id);
+    }
+  };
+  teamsList.addEventListener('click', openTeamsInfo);
+  /////////////Карусель
+  var carusel = function (container, prev, next) {
+    let caruselBlock = document.querySelector(container);
+    let itemLength = caruselBlock.children.length;
+    var prev = document.querySelector(prev);
+    var next = document.querySelector(next);
+
+    const minSteps = -100;
+    const step = 100;
+    const maxSteps = step * itemLength;
+    let currentStep = 0;
+    const reset = maxSteps;
+
+    caruselBlock.style["transform"] = `translateX(${currentStep}vw)`;
+
+    function right(e) {
+      e.preventDefault();
+      if (currentStep < maxSteps) {
+        currentStep += step;
+        caruselBlock.style["transform"] = `translateX(-${currentStep}vw)`;
+      }
+      if (currentStep == maxSteps) {
+        currentStep -= reset;
+        caruselBlock.style["transform"] = `translateX(-${currentStep}vw)`;
+      }
+    };
+
+    function left(e) {
+      e.preventDefault();
+      if (currentStep > minSteps) {
+        currentStep -= step;
+        caruselBlock.style["transform"] = `translateX(-${currentStep}vw)`;
+      }
+      if (currentStep <= minSteps) {
+        currentStep += reset;
+        caruselBlock.style["transform"] = `translateX(-${currentStep}vw)`;
+      }
+    };
+    prev.addEventListener('click', right);
+    next.addEventListener('click', left);
+  };
+  carusel('.slider__list', '.arrow__left', '.arrow__right');
+  ///////Форма
+  const modal = document.querySelector('.delivery__modal');
+  const form = document.querySelector('.delivery__form');
+  const submit = form.querySelector('.order__submit');
+  const modalMsg = modal.querySelector('.delivery__modal-text')
+  const modalClose = modal.querySelector('.delivery__btn');
+
+  function validate (form) { 
+    let valid = true;
+    if (!validateField(form.elements.name)) {
+      valid = false;
+    }
+    if (!validateField(form.elements.phone)) {
+      valid = false;
+    }
+    if (!validateField(form.elements.comment)) {
+      valid = false;
+    }
+    return valid;
   }
-};
+  function validateField (field) {
+    field.nextElementSibling.textContent = field.validationMessage;
+    return field.checkValidity();
+  } 
+
+  function exchange (e) {
+    e.preventDefault();
+    let url = 'https://webdev-api.loftschool.com/sendmail';
+    let urlFail = 'https://webdev-api.loftschool.com/sendmail/fail';
+    if (validate(form)) {
+      var fData = new FormData();
+      fData.append('name', form.elements.name.value);
+      fData.append('phone', form.elements.phone.value);
+      fData.append('comment', form.elements.comment.value);
+      fData.append('to', 'rt@gmail.com');
+  
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.open('POST', url);
+      xhr.send(fData);
+      xhr.addEventListener('load', function () {
+        if (xhr.status >= 400) {
+          console.log(xhr.response.message);
+          modal.style.display = 'flex';
+          overlay.classList.add('overlay');
+          modalMsg.textContent = xhr.response.message;
+  
+        } else {
+          console.log(xhr.response.message);
+          modal.style.display = 'flex';
+          overlay.classList.add('overlay');
+          modalMsg.textContent = xhr.response.message;
+        }
+      })
+    }
+    // return xhr;
+  };
+
+  submit.addEventListener('click', exchange);
+  modalClose.addEventListener('click', function (e) {
+    e.preventDefault();
+    modal.style.display = 'none';
+    overlay.classList.remove('overlay');
+  })
+}
+
+
+
