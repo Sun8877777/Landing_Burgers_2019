0cdad504a8e496d702a5c2f0aaf9f23e00d40088
Alexandr Cheremisin <an-cheremisin@yandex.ru>
2019-06-10 20:44:09 +0300

    Финальная версия v7

--- prod/js/run.js
+++ prod/js/run.js
@@ -271,7 +271,7 @@ window.onload = function () {
         // balloonContent: 'Красивая метка'
       }, {
           iconLayout: 'default#image',
-          iconImageHref: '/img/map-marker.svg',
+          iconImageHref: './img/map-marker.svg',
           iconImageSize: [50, 80],
           iconImageOffset: [-5, -38]
         }),
@@ -282,7 +282,7 @@ window.onload = function () {
         iconContent: '12'
       }, {
           iconLayout: 'default#imageWithContent',
-          iconImageHref: '/img/map-marker.svg',
+          iconImageHref: './img/map-marker.svg',
           iconImageSize: [50, 80],
           iconImageOffset: [-24, -24],
           iconContentOffset: [15, 15],
